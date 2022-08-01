dd587281d57b8fc509b7f5c8ae78cac40eeb22e6
Vitor Alves <vitorcarneiro778@gmail.com>
2022-08-01 16:12:11 -0300

    First class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import bordgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		
+		Position pos = new Position();
 
 	}
 
